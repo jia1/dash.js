ba52774b76afd062c6faa8859fe114d574388662
Jia Yee <jiayeerawr@gmail.com>
2017-11-09 16:31:02 +0800

    Add more constants and imports to BBARule
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- src/streaming/rules/abr/BBARule.js
+++ src/streaming/rules/abr/BBARule.js
@@ -1,6 +1,7 @@
 import FactoryMaker from '../../../core/FactoryMaker';
 import Debug from '../../../core/Debug';
 import SwitchRequest from '../SwitchRequest.js';
+import { LINEAR_F, BBA0 } from './BBAAlgorithm';
 
 function BBARule(config) {
 
@@ -13,6 +14,44 @@ function BBARule(config) {
 
     function getMaxIndex(rulesContext) {
         const switchRequest = SwitchRequest(context).create();
+
+        let bufferLevel = dashMetrics.getCurrentBufferLevel(metrics);
+        const metrics = metricsModel.getReadOnlyMetricsFor(mediaType);
+
+        const mediaInfo = rulesContext.getMediaInfo();
+        const bitrateList = mediaInfo.bitrateList.map(b => b.bandwidth);
+
+        const mediaType = rulesContext.getMediaType();
+
+        const streamInfo = rulesContext.getStreamInfo();
+        const streamId = streamInfo ? streamInfo.id : null;
+        const isDynamic = streamInfo && streamInfo.manifestInfo && streamInfo.manifestInfo.isDynamic;
+
+        const useBufferOccupancyABR = rulesContext.useBufferOccupancyABR();
+
+        const abrController = rulesContext.getAbrController();
+        const throughputHistory = abrController.getThroughputHistory();
+        let latency = throughputHistory.getAverageLatency(mediaType);
+        let safeThroughput = throughputHistory.getSafeAverageThroughput(mediaType, isDynamic);
+        let throughput = throughputHistory.getAverageThroughput(mediaType, isDynamic);
+        let quality;
+
+        switchRequest.reason = switchRequest.reason || {};
+        switchRequest.reason.latency = latency;
+        switchRequest.reason.throughput = throughput;
+
+        // abrController.getTopQualityIndexFor(mediaType, streamId)
+
+        if (!useBufferOccupancyABR) {
+            return switchRequest;
+        }
+
+        if (isNaN(throughput)) {
+            return switchRequest;
+        }
+
+        switchRequest.quality = quality;
+
         return switchRequest;
     }
 
